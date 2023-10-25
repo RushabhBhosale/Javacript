b934aa65ee430c9cf4d19af220f28e5698b562ca
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2023-10-25 10:44:54 +0530

    update

---
+++ Day1/day1.js
@@ -0,0 +1,9 @@
+console.log('Rushabh');
+console.log("Rushabh");
+console.log(`Rushabh`);
+console.log(typeof ('1'));
+console.log(typeof (`1`));
+console.log(typeof ("1"));
+
+console.log(23);
+console.log(typeof (23));
