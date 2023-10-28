0040c69da9c5774409fdf9393377c69dd28dec9a
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2023-10-28 15:21:27 +0530

    Updates

---
+++ Day2/day2.js
@@ -0,0 +1,44 @@
+// console.log("awdiz")
+// console.log(typeof("awdiz"))
+// console.log(23)
+// console.log(typeof(23))
+// console.log("23"+"23")
+// console.log(23+23)
+
+// var num1 = 2
+// var num2 = 3
+
+
+
+// function add(num1, num2) {
+//     return num1 + num2
+// }
+// add(2,3)
+
+// console.log(add(2,3))
+
+// console.log(true)
+// console.log(typeof (true))
+// console.log(false)
+// console.log(typeof (false))
+// console.log(typeof ('false'))
+
+// var name = "rushabh"
+// console.log(name)
+
+// var number1 = 50
+// var number2 = 150   
+
+// console.log(number1 * number2)
+// console.log(number1 - number2)
+// console.log(number1 / number2)
+// console.log(number1 % number2)
+// console.log(number1 + number2)
+
+// console.log('2' == 2)
+// console.log('2' === 2)
+
+var age = 81
+
+console.log(age > 19 && age < 80)
+console.log(age > 19 || age < 80)
