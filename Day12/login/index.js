2af1517f94024022fb3a786ff263f4729eaf3286
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2023-11-23 21:04:18 +0530

    Updates

---
+++ Day12/login/index.js
@@ -0,0 +1,24 @@
+function login(event) {
+   event.preventDefault();
+
+   var email = document.getElementById("email").value;
+   var password = document.getElementById("password").value;
+
+   if (!email || !password) {
+      return alert("All feilds are required")
+   }
+
+   var users = JSON.parse(localStorage.getItem("Users"));
+   console.log(users)
+
+   for (var i = 0; i < users.length; i++) {
+      if (users[i].email == email && users[i].password == password) {
+         document.getElementById("email").value = "";
+         document.getElementById("password").value = "";
+         window.location.href = './../index.html'
+         return alert("Login Successful")
+      }
+   }
+
+   return alert("Please check your email password")
+}
