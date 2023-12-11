b539cf25395d6824f9f09411e1398daf9e0da157
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2023-12-11 13:06:02 +0530

    Updates

--- Day12/register/index.js
+++ Day12/register/index.js
@@ -2,11 +2,8 @@ function register(event) {
     event.preventDefault();
 
     var name = document.getElementById("name").value;
-    console.log(name, "-name")
     var email = document.getElementById("email").value;
-    console.log(email, "- email")
     var password = document.getElementById("password").value;
-    console.log(password, "- password")
 
     if (!name || !email || !password) {
         return alert("All the fields are required")
@@ -27,6 +24,4 @@ function register(event) {
     alert("Registration Successful")
 
     window.location.href = './../login/index.html';
-
-
 }   
